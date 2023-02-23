487f5bfa417a8ef65059caf5bff71e5eb09791fd
ivaod <82960898+ivaod@users.noreply.github.com>
2023-02-22 20:51:34 -0500

    Update UserLogin.js

--- testcases/UserLogin.js
+++ testcases/UserLogin.js
@@ -11,21 +11,33 @@ it("Mouse hover and login", function()
 
     browser.manage().window().maximize();
     
+    //hoover on "Hello, sign in" section
+    
     var helloSignInSection = element(by.xpath("/html/body/div[1]/header/div/div[1]/div[3]/div/a[2]"));
 
     browser.actions().mouseMove(helloSignInSection).perform();
+    
+    //click on "Sign in" button
 
     var singInButton= element(by.xpath("//span[@class='nav-action-inner']"));
     singInButton.click;
+    
+    //enter valid email or phone number credentials
 
     var emailOrNumberToSignIn= element(by.xpath("//*[@id='ap_email']"));
     emailOrNumberToSignIn.sendKeys("valid email or phone number");
+    
+    //click on "Continue" button
 
     var continueButton= element(by.xpath("//input[@id='continue']"));
     continueButton.click;
+    
+    //enter valid password
 
     var  passwordButton= element(by.xpath("//input[@id='ap_password']"));
     passwordButton.sendKeys("valid password");
+    
+    //click on "Sign in" button
 
     var signInSubmitButton= element(by.xpath("//input[@id='signInSubmit']"));
     signInSubmitButton.click;

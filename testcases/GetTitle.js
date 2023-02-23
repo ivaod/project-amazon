91e6b9693796bedf626b5457d21519fac7e0acd5
ivaod <82960898+ivaod@users.noreply.github.com>
2023-02-22 20:45:20 -0500

    Update GetTitle.js

--- testcases/GetTitle.js
+++ testcases/GetTitle.js
@@ -1,15 +1,14 @@
 
 import {browser} from "protractor"
 
-describe("Test1", function(){
-it("validate title", function(){
+describe("Test", function(){
+it("Validate title", function(){
     browser.get('https://www.amazon.com');
     browser.sleep(5000);
     browser.manage().window().maximize();
 
-    expect(browser.getCurrentUlr()).toBe('https://www.amazon.com');
-
-    expect(browser.getTitle()).toBe('Amazon.com. Spend less. Smile more.');
+   expect(browser.getTitle()).toBe('Amazon.com. Spend less. Smile more.');
+    
 });
 
 
